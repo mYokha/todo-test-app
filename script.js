c541e46ff6b3efe41c0a7fdba93d9e680b465ea4
Max Yokha <max.yokha@gmail.com>
2017-08-28 04:01:44 +0300

    revealing module pattern used

--- script.js
+++ script.js
@@ -1,133 +1,143 @@
-(function () {
-  let app = {
-    tasks: [],
+let app = (function () {
+  let tasks = [];
+  let oreder;
 
-    init () {
-      if (localStorage.getItem('tasks')) {
-        this.tasks = JSON.parse(localStorage.getItem('tasks'));
-      }
-      const amount = this.tasks.length;
-      this.order = amount ? amount + 1 : 1;
+  //initialize
+  if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  const amount = tasks.length;
+  order = amount ? amount + 1 : 1;
 
-      this.cacheDOM();
-      this.bindEvents();
-      this.render();
-    },
+  //cache DOM
+  const form = document.querySelector('form');
+  const input = document.getElementById('text-input');
+  const list = document.getElementById('todo-list');
+  const item = list.getElementsByClassName('item');
+  const sortByName = document.getElementById('sort-by-name');
+  const sortByOrder = document.getElementById('sort-by-order');
+  const deleteButtons = document.getElementsByClassName('remove-item');
+  const items = document.getElementsByClassName('item');
 
-    cacheDOM () {
-      this.form = document.querySelector('form');
-      this.input = document.getElementById('text-input');
-      this.list = document.getElementById('todo-list');
-      this.item = this.list.getElementsByClassName('item');
-      this.sortByName = document.getElementById('sort-by-name');
-      this.sortByOrder = document.getElementById('sort-by-order');
-      this.deleteButtons = document.getElementsByClassName('remove-item');
-      this.items = document.getElementsByClassName('item');
-    },
+  //bind events
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    addTask();
+  });
 
-    bindEvents () {
-      this.form.addEventListener('submit', e => {
-        e.preventDefault();
-        this.addTask();
+  //let's finally render this thing
+  render();
+
+  sortByOrder.addEventListener('click', () => sortById());
+  sortByName.addEventListener('click', () => sortByValue());
+
+  list.addEventListener('click', e => {
+    if (!e.target.classList.contains('remove-item')) return;
+    const taskId = parseInt(e.target.parentElement.dataset.id, 10);
+    deleteTask(taskId);
+  });
+
+  list.addEventListener('click', e => {
+    if (!e.target.matches('input')) return;
+    const taskId = parseInt(e.target.parentElement.parentElement.dataset.id, 10);
+    toggleDone(taskId);
+  });
+
+  function addTask (value = input.value) {
+    if (value) {
+      tasks.push({
+        id: order,
+        value,
+        checked: false
       });
 
-      this.sortByOrder.addEventListener('click', () => this.sortById());
-      this.sortByName.addEventListener('click', () => this.sortByValue());
-
-      this.list.addEventListener('click', e => {
-        if (!e.target.classList.contains('remove-item')) return;
-        const taskId = parseInt(e.target.parentElement.dataset.id, 10);
-        this.deleteTask(taskId);
-      });
-
-      this.list.addEventListener('click', e => {
-        if (!e.target.matches('input')) return;
-        const taskId = parseInt(e.target.parentElement.parentElement.dataset.id, 10);
-        this.toggleDone(taskId);
-      });
-    },
-
-    addTask () {
-      if (this.input.value.length) {
-        this.tasks.push({
-          id: this.order,
-          value: this.input.value,
-          checked: false
-        });
-
-        this.form.reset();
-        this.order++;
-        this.render();
-      } else {
-        alert('You\'ve tried to enter an empty value!');
-      }
-    },
-
-    deleteTask (id) {
-      this.tasks = this.tasks.filter(task => task.id !== id);
-
-      this.order = 1;
-      this.tasks.forEach(task => {
-        task.id = this.order;
-        this.order++;
-      });
-
-      this.render();
-    },
-
-    toggleDone (id) {
-      this.tasks = this.tasks.map(task => {
-        if (task.id === id) {
-          task.checked = !task.checked;
-        }
-
-        return task;
-      });
-
-      this.render();
-    },
-
-    sortById () {
-      this.tasks = this.tasks.sort((a, b) => a.id - b.id);
-      this.render();
-    },
-
-    sortByValue () {
-      this.tasks = this.tasks.sort((a, b) => a.value > b.value);
-      this.render();
-    },
-
-    templateItem (item) {
-      return `
-        <div class="item df" data-id="${item.id}">
-          <div class="df checkbox">
-            <input type="checkbox" ${item.checked ? 'checked' : ''}>
-          </div>
-          <div class="item-value">
-            <span class="item-id">${item.id}.</span><span${item.checked ? ' class="item-checked"' : ''}>${item.value}</span>
-          </div>
-          <div class="remove-item">✕</div>
-        </div>
-      `;
-    },
-
-    render () {
-      let data = {
-        tasks: this.tasks,
-        html: ''
-      };
-
-      data.tasks.forEach(task => {
-        data.html += this.templateItem(task);
-      });
-
-      this.list.innerHTML = data.html;
-
-      localStorage.setItem('tasks', JSON.stringify(this.tasks));
-
-      console.table(this.tasks);
+      form.reset();
+      order++;
+      render();
+    } else {
+      alert(`You've tried to enter an empty value!`);
     }
   };
 
-  app.init();
+  function deleteTask (id) {
+    if (!(/^[1-9]\d*$/.test(id))){
+      alert (`Must be positive integers only!`);
+      return;
+    }
+    if (id > tasks.length) {
+        alert(`No task with this id!`);
+        return;
+    }
+
+    tasks = tasks.filter(task => task.id !== id);
+
+    order = 1;
+    tasks.forEach(task => {
+      id = order;
+      order++;
+    });
+
+    render();
+  };
+
+  function toggleDone (id) {
+    tasks = tasks.map(task => {
+      if (task.id === id) {
+        task.checked = !task.checked;
+      }
+
+      return task;
+    });
+
+    render();
+  };
+
+  function sortById () {
+    tasks = tasks.sort((a, b) => a.id - b.id);
+    render();
+  };
+
+  function sortByValue () {
+    tasks = tasks.sort((a, b) => a.value > b.value);
+    render();
+  };
+
+  function templateItem (item) {
+    return `
+      <div class="item df" data-id="${item.id}">
+        <div class="df checkbox">
+          <input type="checkbox" ${item.checked ? 'checked' : ''}>
+        </div>
+        <div class="item-value">
+          <span class="item-id">${item.id}.</span><span${item.checked ? ' class="item-checked"' : ''}>${item.value}</span>
+        </div>
+        <div class="remove-item">✕</div>
+      </div>
+    `;
+  };
+
+  function render () {
+    let data = {
+      tasks,
+      html: ''
+    };
+
+    data.tasks.forEach(task => {
+      data.html += templateItem(task);
+    });
+
+    list.innerHTML = data.html;
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    console.table(tasks);
+  };
+
+  return {
+    addTask,
+    deleteTask,
+    sortById,
+    sortByValue
+  };
+
 })();
