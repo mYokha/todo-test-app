a7b18ac1015a446d105394dd3c00ff6b66d2cc0a
Max Yokha <max.yokha@gmail.com>
2017-09-10 01:18:26 +0300

    counter functionality in a separate module. rename js files.

---
+++ counter.js
@@ -0,0 +1,39 @@
+// Counter component
+const counter = (function (){
+  let count = {
+    general: 0,
+    done: 0,
+    active: 0
+  };
+
+  // Cache DOM
+  const allTasksElem = document.getElementById('all-tasks');
+  const activeTasksElem = document.getElementById('active-tasks');
+  const doneTasksElem = document.getElementById('done-tasks');
+
+  function render () {
+    allTasksElem.innerHTML = count.general;
+    activeTasksElem.innerHTML = count.active;
+    doneTasksElem.innerHTML = count.done;
+  }
+
+  function setCounter (newTasks) {
+    count.general = newTasks.length;
+    count.done = 0;
+    count.active = 0;
+
+    newTasks.forEach(task => {
+      if (task.checked){
+        count.done ++;
+      } else {
+        count.active ++;
+      }
+    });
+    render ();
+    return count;
+  }
+
+  return {
+    setCounter: setCounter
+  };
+})();
